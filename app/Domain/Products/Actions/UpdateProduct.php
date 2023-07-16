03187959dd34a5ad49c02460769d3d165b134f85
Kevin Vargas <vargaskevin19@gmail.com>
2023-07-16 12:05:50 -0500

    fix: update product

--- app/Domain/Products/Actions/UpdateProduct.php
+++ app/Domain/Products/Actions/UpdateProduct.php
@@ -5,6 +5,7 @@ namespace App\Domain\Products\Actions;
 use App\Domain\Products\Models\Product;
 use App\Support\Actions\Action;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class UpdateProduct implements Action
 {
@@ -14,18 +15,26 @@ class UpdateProduct implements Action
      */
     public static function execute(array $params): bool
     {
-        $fields = $params['fields'];
-        $product = $params['product'];
+        $response = false;
+        try {
+            $fields = $params['fields'];
+            $product = $params['product'];
 
-        if (array_key_exists('image', $fields) && $fields['image'] !== null) {
-            if ($product->image) {
-                Storage::disk('public')->delete($product->image);
+            if (array_key_exists('image', $fields) && $fields['image'] !== null) {
+                if ($product->image) {
+                    Storage::disk('public')->delete($product->image);
+                }
+                $fields['image'] = Storage::disk('public')->putFile('products_images', $fields['image']);
+            } else {
+                $fields['image'] = $product->image;
             }
-            $fields['image'] = Storage::disk('public')->putFile('products_images', $fields['image']);
-        } else {
-            $fields['image'] = $product->image;
-        }
 
-        return $product->update($fields);
+            $fields['slug'] = Str::slug($fields['name'], '-', 'es');
+
+            $response = $product->update($fields);
+        } catch (\Exception $exception) {
+            logger()->error($exception->getMessage());
+        }
+        return $response;
     }
 }
