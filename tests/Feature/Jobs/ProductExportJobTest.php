f45acf58c3684dfd911b3a375841615e77a9b80d
Kevin Vargas <vargaskevin19@gmail.com>
2023-07-15 22:47:01 -0500

    fix: export product test case

--- tests/Feature/Jobs/ProductExportJobTest.php
+++ tests/Feature/Jobs/ProductExportJobTest.php
@@ -4,7 +4,9 @@ namespace Tests\Feature\Jobs;
 
 use App\Domain\Users\Models\User;
 use App\Jobs\ProductExportJob;
+use App\Mail\ExportMail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
@@ -15,7 +17,10 @@ class ProductExportJobTest extends TestCase
     public function test_generate_export_file_ok(): void
     {
         Storage::fake("public");
+        Mail::fake();
+
         (new ProductExportJob(User::factory()->admin()->create()))->handle();
         Storage::disk('public')->assertExists('exports/export_productos.csv');
+        Mail::assertSent(ExportMail::class);
     }
 }
